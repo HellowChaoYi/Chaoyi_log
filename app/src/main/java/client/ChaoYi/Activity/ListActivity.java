e9b18b37d4f9635f0129acd570953fb26d127d38
HellowChaoYi <469331533@qq.com>
2019-03-14 23:33:06 +0800

    updata home

--- app/src/main/java/client/ChaoYi/Activity/ListActivity.java
+++ app/src/main/java/client/ChaoYi/Activity/ListActivity.java
@@ -2,31 +2,33 @@ package client.ChaoYi.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import client.ChaoYi.R;
 import client.ChaoYi.Ui.CustomTitleBar;
+import client.ChaoYi.Ui.until.StatusBarUtil;
 
 public class ListActivity extends AppCompatActivity {
-    private TextView mTitleTextView;
-    private Button mBackwardbButton;
-    private Button mForwardButton;
-    private FrameLayout mContentLayout;
+    private CustomTitleBar mcustomTitlebar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-//        mTitleTextView = (TextView) findViewById(R.id.text_title);
-//        mContentLayout = (FrameLayout) findViewById(R.id.layout_content);
-//        mBackwardbButton = (Button) findViewById(R.id.button_backward);
-//        showBackwardView(R.string.text_back,true);
-//        showForwardView(R.string.text_forward,true);
-        CustomTitleBar titleBar = findViewById(R.id.titlebar);
-        titleBar.setLeftIconOnClickListener(new View.OnClickListener() {
+
+        mcustomTitlebar = findViewById(R.id.titlebar);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        CustomTitleBar.LayoutParams lp = new CustomTitleBar.LayoutParams(mcustomTitlebar.getLayoutParams());
+        lp.setMargins(0, 10000, 10, 0);
+
+        mcustomTitlebar.setLeftIconOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(ListActivity.this, "左边", Toast.LENGTH_SHORT).show();
